b5553875fe5cd49720dddf21014cd4ebcb91e912
Salmani Ziya <salmaniziya2214@gmail.com>
2026-01-02 18:53:17 +0530

    This is a practice code for checking its a leap year or not

--- Basics/Triangle.py
+++ Basics/Triangle.py
@@ -5,7 +5,7 @@ def is_right_triangle(a, b, c):
     return (a**2 + b**2 == c**2) or \
            (a**2 + c**2 == b**2) or \
            (b**2 + c**2 == a**2)
-
+# this is the pythin code
 # Taking input
 a = int(input("Enter side 1: "))
 b = int(input("Enter side 2: "))
---
+++ Basics/leapyear.py
@@ -0,0 +1,8 @@
+year = int (input("Enter the year to check if its a leap or not "))
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(year, "is a leap year")
+        else:
+            print(year, "is not a leap year")
+   
