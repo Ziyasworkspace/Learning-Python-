2ac12c57881afffe5ba4a5d9ac5e225474bc6705
Salmani Ziya <salmaniziya2214@gmail.com>
2026-01-02 18:29:09 +0530

    This is the Practice code of 2nd january

---
+++ Basics/prac.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the Second number:"))
+num3=int(input("Enter the Third number:"))
+if num1>num2 and num1>num3 :
+    print("The first number is  greatest")
+elif num2>num1 or num2>num3 :
+    print(" The second number is greatest")    
+else:
+    print("The third number is greatest")
+
